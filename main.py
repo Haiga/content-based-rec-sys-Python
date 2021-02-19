0d69518e0f1c7ed62f9115eb46406d39741c085f
Pedro <pedro_rga@hotmail.com>
2021-02-19 12:13:27 -0300

    Content based on genres, years, releases, runtimes, languages, countries,              imdb_ratings

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 import re
 import json
+from utils import indexTwoSets, readFile, writePredict
+import numpy as np
 
 with open("content.csv", "r", encoding="utf8") as content_file:
     content = content_file.readlines()[1:]
 
-
-def my_prepro(info_dict):
-    info_dict['Genre'].split(",")
-
-
 first = True
 items_infos = {}
-genres = set()
-years = set()
-releases = set()
-runtimes = set()
-languages = set()
-countries = set()
-imdb_ratings = set()
+genres = {}
+years = {}
+releases = {}
+runtimes = {}
+languages = {}
+countries = {}
+imdb_ratings = {}
 for line in content:
     pattern = re.compile('i([0-9]*),(.*)')
     groups = pattern.match(line).groups()
@@ -26,45 +23,128 @@ for line in content:
     dict_content = json.loads(json_content)
     if first:
         comp = sorted(dict_content.keys())
-        print(dict_content.keys())
+        # print(dict_content.keys())
         first = False
-    # else:
-    #     if sorted(dict_content.keys()) != comp:
-    #         print(dict_content.keys())
-    #         print(item_id)
-    #         print("diff")
-    # break
+
+
+    def add_to(any_dict, origin_dict, key, value, split=None):
+        if key not in origin_dict:
+            return
+        if split is None:
+            new_key = key
+            if new_key == "N/A":
+                return
+            if new_key not in any_dict:
+                any_dict.setdefault(origin_dict[new_key], [value])
+            else:
+                any_dict[origin_dict[new_key]].append(value)
+        else:
+            sp = origin_dict[key].split(split)
+            for s in sp:
+                new_key = s.strip()
+                if new_key == "N/A":
+                    continue
+                if new_key not in any_dict:
+                    any_dict.setdefault(new_key, [value])
+                else:
+                    any_dict[new_key].append(value)
+
+
+    item_id = int(item_id)
     items_infos[item_id] = dict_content
-    if 'Genre' in dict_content:
-        sp = dict_content['Genre'].split(",")
-        for genre in sp:
-            if genre.strip() != "N/A":
-                genres.add(genre.strip())
-    if 'Year' in dict_content:
-        if dict_content['Year'] != "N/A":
-            years.add(dict_content['Year'])
-    if 'Released' in dict_content:
-        if dict_content['Released'] != "N/A":
-            releases.add(dict_content['Released'].split(" ")[-1])
-    if 'Runtime' in dict_content:
-        if dict_content['Runtime'] != "N/A":
-            runtimes.add(dict_content['Runtime'].split(" ")[0])
-    if 'Language' in dict_content:
-        if dict_content['Language'] != "N/A":
-            for lang in dict_content['Language'].split(","):
-                languages.add(lang.strip())
-    if 'Country' in dict_content:
-        if dict_content['Country'] != "N/A":
-            for count in dict_content['Country'].split(","):
-                countries.add(count.strip())
-    if 'imdbRating' in dict_content:
-        if dict_content['imdbRating'] != "N/A":
-            imdb_ratings.add(dict_content['imdbRating'])
-# print(items_infos)
-print(genres)
-print(years)
-print(releases)
-print(runtimes)
-print(languages)
-print(countries)
-print(imdb_ratings)
+    add_to(genres, dict_content, 'Genre', item_id, split=",")
+    add_to(years, dict_content, 'Year', item_id)
+    add_to(releases, dict_content, 'Released', item_id)
+    add_to(runtimes, dict_content, 'Runtime', item_id)
+    add_to(languages, dict_content, 'Language', item_id, split=",")
+    add_to(countries, dict_content, 'Country', item_id, split=",")
+    add_to(imdb_ratings, dict_content, 'imdbRating', item_id)
+
+# print(genres)
+# print(years)
+# print(releases)
+# print(runtimes)
+# print(languages)
+# print(countries)
+# print(imdb_ratings)
+
+keys_genres = genres.keys()
+keys_years = years.keys()
+keys_releases = releases.keys()
+keys_runtimes = runtimes.keys()
+keys_languages = languages.keys()
+keys_countries = countries.keys()
+keys_imdb_ratings = imdb_ratings.keys()
+
+num_factors = 0
+for keys_conj in [keys_genres, keys_years, keys_releases, keys_runtimes, keys_languages, keys_countries,
+                  keys_imdb_ratings]:
+    num_factors += len(keys_conj)
+
+items_ids, _ = indexTwoSets(items_infos, items_infos)
+# print(len(items_ids))
+# print(num_factors)
+
+users_dict, items_dict, u_i = readFile("ratings.csv")
+# print(len(users_dict))
+users_ids, _ = indexTwoSets(users_dict, users_dict)
+
+items_vectors = np.zeros((len(items_ids), num_factors))
+users_vectors = np.zeros((len(users_ids), num_factors))
+
+cont = 0
+for conj in [genres, years, releases, runtimes, languages, countries,
+             imdb_ratings]:
+    keys_conj = conj.keys()
+    for key in keys_conj:
+        for i in conj[key]:
+            items_vectors[items_ids[i]][cont] = 1
+        cont += 1
+
+for u in u_i:
+    for i in u_i[u]:
+        if i in items_ids:
+            users_vectors[users_ids[u]] = users_vectors[users_ids[u]] + items_vectors[items_ids[i]] * u_i[u][i]
+
+# Iremos varrer o conjunto de teste, se o usuário ou item não existir nos conjuntos
+# setamos 8 para o rating deles,
+# TODO calcular a media por usuário e a média global, quando não tiver utilizá-los
+
+from numpy import dot
+from numpy.linalg import norm
+
+preds = []
+users_test, items_test, u_i_test = readFile("targets.csv", type_return="array", type="test")
+preds2 = []
+for u_i in u_i_test:
+    u, i = u_i
+
+    if u in users_ids and i in items_ids:
+        a = users_vectors[users_ids[u]]
+        b = items_vectors[items_ids[i]]
+        norma_den = norm(a) * norm(b)
+        if norma_den == 0:
+            preds.append(8)
+            # preds.append(0)
+            preds2.append(1)
+        else:
+            cos_sim = dot(a, b) / (norma_den)
+            preds.append(cos_sim * 10)
+            # preds.append(cos_sim)
+            preds2.append(0)
+    else:
+        cos_sim = 8
+        # cos_sim = 0
+        preds.append(cos_sim)
+        preds2.append(1)
+
+# predsT = np.array(preds)[np.array(preds2) == 0]
+# preds = preds + np.mean(predsT) * np.array(preds2)
+# min_preds = np.min(preds)
+# max_preds = np.max(preds)
+#
+# npred = (preds - min_preds)/(max_preds - min_preds)
+# writePredict("results.csv", u_i_test, npred*10)
+
+
+writePredict("results.csv", u_i_test, preds)
---
+++ utils.py
@@ -0,0 +1,191 @@
+import re
+
+
+def indexTwoSets(first_set, second_set):
+    """
+    Retorna a indexação dos dados nos conjuntos first_set e second_set (essa indexação pode ser utilizada para acessar a
+    posição de um elemento do conjunto em um vetor indexado a partir de zero
+    Parameters
+    ----------
+    first_set: set or dict
+        É um conjunto de dados únicos
+    second_set: set or dict
+        É um conjunto de dados únicos
+    Returns
+    -------
+        indexes, not_in_first_set
+        indexes:
+            Junta os dados do first_set com o second_set, e para cada item única desse conjunto é assinalado um índice
+        not_in_first_set: dict
+            Contém as chaves que estão no second_set mas não no first_set
+    """
+
+    indexes = {}
+    count = 0
+    for u in first_set:
+        if u not in indexes:
+            indexes.setdefault(u, count)
+            count += 1
+
+    not_in_first_set = {}
+    for u in second_set:
+        if u not in indexes:
+            indexes.setdefault(u, count)
+            count += 1
+            not_in_first_set.setdefault(u, 0)
+
+    return indexes, not_in_first_set
+
+
+def readFile(name_file, type="train", ignore_first_line=True, users_to_add=None, items_to_add=None,
+             intersect_or_union='intersect', type_return='dict'):
+    """
+    Essa função lê um arquivo de usuários itens, com ou sem ratings e timestamp
+    Para arquivos do tipo train é retornado um set com os usuários, um set com os itens, e um terceiro retorno
+    Se type_return for 'dict' o terceiro retorno é um dicionário com chaves para usuários, e cada usuário contém
+    um dicionário para seus itens e cada item indexa o rating do usuário chave para tal item
+    Se type_return for 'array' o terceiro retorno é um array com pares de user,item (type == test) ou trios de
+    user,item,rating (type == train)
+    Parameters
+    ----------
+    name_file : str
+        O nome do arquivo a ser lido.
+    type : {'train', 'test'}, optional
+        O tipo do arquivo a ser lido.
+            Se 'train' cada linha do arquivo deve ter o formato u{user_id}:i{item_id},{rating},{timestamp}
+            Se 'test' cada linha do arquivo deve ter o formato u{user_id}:i{item_id}
+    ignore_first_line : boolean, optional
+        Se True ignora a primeira linha do arquivo a ser lido
+    users_to_add: set or dict, optional
+        Se esse parâmetro for utilizado observe também os parâmetros items_to_add e intersect_or_union
+        A saída irá conter apenas usuários em users_to_add
+            Pode conter usuários que não estão em users_to_add se (items_to_add != None & intersect_or_union == 'union')
+    items_to_add: set or dict, optional
+        Se esse parâmetro for utilizado observe também os parâmetros items_to_add e intersect_or_union
+        A saída irá conter apenas itens em items_to_add
+            Pode conter itens que não estão em items_to_add se (users_to_add != None & intersect_or_union == 'union')
+    intersect_or_union : {'intersect', 'union'}, optional
+        Esse parâmetro só é utilizado quando users_to_add e items_to_add são ambos não nulos
+        Se 'intersect' a saída ignora as linhas com usuários que não estão em users_to_add & ignora as linhas com itens que
+            não estão em items_to_add.
+        Se 'union' a saída ignora as linhas com usuários que não estão em users_to_add a menos que o item da linha
+            contenha algum item em items_to_add. Ignora também as linhas com items que não estão em items_to_add a menos
+            que o usuário da linha contenha algum usuário em users_to_add.
+    type_return : {'array', 'dict'}, optional
+        Esse parâmetro indica se o terceiro item do retorno é um array ou um dicionário
+        Se type_return for 'dict' o terceiro retorno é um dicionário com chaves para usuários, e cada usuário contém
+        um dicionário para seus itens e cada item indexa o rating do usuário chave para tal item
+        Se type_return for 'array' o terceiro retorno é um array com pares de user,item (type == test) ou trios de
+        user,item,rating (type == train)
+    """
+
+    with open(name_file, 'r') as file:
+        lines = file.readlines()
+
+    if ignore_first_line:
+        lines = lines[1:]
+
+    if type == "test":
+        p = re.compile('u([0-9]*):i([0-9]*)')
+    elif type == "train":
+        p = re.compile('u([0-9]*):i([0-9]*),([0-9]*),([0-9]*)')
+    else:
+        raise Exception("Invalid type of file")
+
+    all_users = set()
+    all_items = set()
+
+    users_items_map = {}
+
+    cont_removed_ratings = 0
+
+    add_all = users_to_add is None and items_to_add is None
+    add_by_items = users_to_add is None and not items_to_add is None
+    add_by_users = not users_to_add is None and items_to_add is None
+
+    array_user_item_pairs = []
+
+    for line in lines:
+        m = p.match(line)
+
+        u = int(m.groups()[0])
+        i = int(m.groups()[1])
+
+        if type == "test":
+            r = 0
+        else:
+            r = int(m.groups()[2])
+
+        all_users.add(u)
+        all_items.add(i)
+
+        should_be_added = False
+        if add_all:
+            should_be_added = True
+        elif add_by_items and add_by_users:
+            if intersect_or_union == 'intersect':
+                if u in users_to_add and i in items_to_add:
+                    should_be_added = True
+            if intersect_or_union == 'union':
+                if u in users_to_add or i in items_to_add:
+                    should_be_added = True
+        elif add_by_users:
+            if u in users_to_add:
+                should_be_added = True
+        elif add_by_items:
+            if i in items_to_add:
+                should_be_added = True
+
+        if should_be_added:
+            if type_return == 'array' and type == 'train':
+                array_user_item_pairs.append((u, i, r))
+            elif type_return == 'array' and type == 'test':
+                array_user_item_pairs.append((u, i))
+            else:
+                users_items_map.setdefault(u, {})
+                users_items_map[u].setdefault(i, r)
+        else:
+            cont_removed_ratings += 1
+
+    # print(f"{cont_removed_ratings} removed ratings")
+    if type_return == 'array':
+        return all_users, all_items, array_user_item_pairs
+    return all_users, all_items, users_items_map
+
+
+def writePredict(name_file_output, users_itens, predicts, header="UserId:ItemId,Prediction", verbose=False, max_punctuation=10, min_punctuation=0):
+    """
+    Parameters
+    ----------
+    name_file_output: str Nome do arquivo de saída
+    users_itens: array de pares (u, i)
+    predicts: array de ratings preditos para todos os pares (u, i)
+    header: str Cabeçalho do arquivo de saída
+    verbose: bool Escreve as predições na saída padrão
+    max_punctuation: int, optional Se alguma predição fica acima de max_punctuation, a predição é convertida para esse máximo
+    min_punctuation: int, optional Se alguma predição fica abaixo de min_punctuation, a predição é convertida para esse mínimo
+    Returns
+    -------
+    """
+    if verbose:
+        for u_i, p in zip(users_itens, predicts):
+            u = u_i[0]
+            i = u_i[1]
+            if p > max_punctuation:
+                print("u" + str(u).zfill(7) + ":" + "i" + str(i).zfill(7) + "," + str(10))
+            elif p < min_punctuation:
+                print("u" + str(u).zfill(7) + ":" + "i" + str(i).zfill(7) + "," + str(0))
+            else:
+                print("u" + str(u).zfill(7) + ":" + "i" + str(i).zfill(7) + "," + str(p))
+    else:
+        with open(name_file_output, 'w') as file:
+            file.write(header + "\n")
+            for u_i, p in zip(users_itens, predicts):
+                u = u_i[0]
+                i = u_i[1]
+                if p > max_punctuation:
+                    file.write("u" + str(u).zfill(7) + ":" + "i" + str(i).zfill(7) + "," + str(10) + "\n")
+                elif p < min_punctuation:
+                    file.write("u" + str(u).zfill(7) + ":" + "i" + str(i).zfill(7) + "," + str(0) + "\n")
+                else:
+                    file.write("u" + str(u).zfill(7) + ":" + "i" + str(i).zfill(7) + "," + str(p) + "\n")
